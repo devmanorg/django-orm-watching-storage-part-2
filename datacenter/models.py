54e7ae3c5608844b9e28aa8685296c4d3ed4eadc
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-09-02 19:14:13 +0300

    1

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.utils.timezone import localtime
+from datetime import timezone
+import datetime
 
 
 class Passcard(models.Model):
@@ -10,7 +13,7 @@ class Passcard(models.Model):
     def __str__(self):
         if self.is_active:
             return self.owner_name
-        return f'{self.owner_name} (inactive)'
+        return f"{self.owner_name} (inactive)"
 
 
 class Visit(models.Model):
@@ -20,11 +23,35 @@ class Visit(models.Model):
     leaved_at = models.DateTimeField(null=True)
 
     def __str__(self):
-        return '{user} entered at {entered} {leaved}'.format(
+        return "{user} entered at {entered} {leaved}".format(
             user=self.passcard.owner_name,
             entered=self.entered_at,
-            leaved=(
-                f'leaved at {self.leaved_at}'
-                if self.leaved_at else 'not leaved'
-            )
+            leaved=(f"leaved at {self.leaved_at}" if self.leaved_at else "not leaved"),
         )
+
+    def get_duration(self):
+        visit_time = (
+            localtime(self.leaved_at) - localtime(self.entered_at)
+        ).total_seconds()
+        return visit_time
+
+    def format_duration(self, visit_time):
+        seconds = visit_time.total_seconds()
+        hours = seconds // 3600
+
+        int(hours)
+
+        minutes = (seconds % 3600) // 60
+        int(minutes)
+        return f"{int(hours)}ч {int(minutes)}мин"
+
+    def is_visit_long(self):
+        minutes = 60
+        duration = self.get_duration()
+        return duration // 60 > minutes
+
+    def is_visit_long_now(self):
+        now = datetime.datetime.now(timezone.utc)
+        minutes = 60
+        duration = (now - localtime(self.entered_at)).total_seconds()
+        return duration // 60 > minutes
--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,21 +1,27 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils.timezone import localtime
+from datetime import timezone
+import datetime
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
+    this_passcard_visits = []
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    visits = Visit.objects.filter(passcard=passcard)
 
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
-    context = {
-        'passcard': passcard,
-        'this_passcard_visits': this_passcard_visits
-    }
-    return render(request, 'passcard_info.html', context)
+    for visit in visits:
+        entered = localtime(visit.entered_at)
+        leave = localtime(visit.leaved_at)
+
+        this_passcard_visits.append(
+            {
+                "entered_at": entered,
+                "duration": visit.format_duration(leave - entered),
+                "is_strange": visit.is_visit_long(),
+            }
+        )
+
+    context = {"passcard": passcard, "this_passcard_visits": this_passcard_visits}
+    return render(request, "passcard_info.html", context)
