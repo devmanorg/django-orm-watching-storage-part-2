7dc9eeeedaed50bba5c7740d7fc7fc38a77b49a7
Zney4 <134953901+Zney4@users.noreply.github.com>
2024-09-27 18:23:52 +0300

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,8 +2,6 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
-from datetime import timezone
-import datetime
 
 
 def passcard_info_view(request, passcode):
