038bbb0df73bae497f46b624abf3892593ce191e
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-02-07 14:50:52 +0300

    Измменение файлов view

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,21 +1,14 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
-
+from datacenter.time_utils import format_duration, get_duration
 
 def storage_information_view(request):
     leaved_at_visits = Visit.objects.filter(leaved_at=None)
 
-    def format_duration(duration):
-        total_seconds = int(duration.total_seconds())
-        hours = total_seconds // 3600
-        remainder = total_seconds % 3600
-        minutes = remainder // 60
-        return f'{hours}ч {minutes}мин'
-
     non_closed_visits = []
     for visit in leaved_at_visits:
-        duration = visit.get_duration()
+        duration = get_duration()
         visit_info = {
             'who_entered': visit.passcard.owner_name,
             'entered_at': visit.entered_at.strftime('%d-%m-%Y %H:%M'),
