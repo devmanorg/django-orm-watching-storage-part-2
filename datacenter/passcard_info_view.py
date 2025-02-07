ad0099475b2f7f3688d5588b0c42a22ac73b15d1
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-02-07 14:50:58 +0300

    Измменение файлов view

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,28 +2,21 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from datacenter.time_utils import format_duration, is_visit_long, get_duration
 
 
 def passcard_info_view(request, passcode):
     passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
 
-    def format_duration(duration):
-        total_seconds = int(duration.total_seconds())
-        hours = total_seconds // 3600
-        remainder = total_seconds % 3600
-        minutes = remainder // 60
-        seconds = remainder % 60
-        return f'{hours}ч {minutes}мин {seconds}сек'
-
     this_passcard_visits = []
 
     for visit in visits:
-        duration = visit.get_duration()
+        duration = get_duration(visit)
         this_passcard_visits.append({
             'entered_at': visit.entered_at,
             'duration': format_duration(duration),
-            'is_strange': visit.is_visit_long()
+            'is_strange': is_visit_long(visit)
         })
 
     context = {
