5ce44796800e4157ba08c833e943fe19880576c5
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-02-07 14:58:22 +0300

    Изменения в time_utils.py

--- datacenter/time_utils.py
+++ datacenter/time_utils.py
@@ -3,26 +3,25 @@ from django.utils import timezone
 from datetime import timedelta
 
 
+
+SECONDS_IN_MINUTE = 60
+SECONDS_IN_HOUR = 3600
+
 visit = Visit(entered_at=timezone.now(), leaved_at=None)
 
 
 def get_duration(visit):
     current_time = timezone.localtime()
-
-    if visit.leaved_at:
-        duration = visit.leaved_at - visit.entered_at
-    else:
-        duration = current_time - visit.entered_at
-
-    return (duration)
+    duration = (visit.leaved_at or current_time) - visit.entered_at
+    return duration
 
 
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
-    hours = total_seconds // 3600
-    remainder = total_seconds % 3600
-    minutes = remainder // 60
-    seconds = remainder % 60
+    hours = total_seconds // SECONDS_IN_HOUR
+    remainder = total_seconds % SECONDS_IN_HOUR
+    minutes = remainder // SECONDS_IN_MINUTE
+    seconds = remainder % SECONDS_IN_MINUTE
     return f'{hours}ч {minutes}мин {seconds}сек'
 
 
