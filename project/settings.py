a917268690c8667a0a82ca09a2c311903a912c3d
tabruno27 <114284727+tabruno27@users.noreply.github.com>
2025-02-20 13:31:19 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ INSTALLED_APPS = ['datacenter']
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost'])
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
